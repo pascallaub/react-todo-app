ef025ae39b1c16b797d06f67ba53046599900cd2
pascallaub <nutritionexpertition@gmail.com>
2025-01-31 13:51:13 +0100

    added checkbox linethrough

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,22 @@
-function TodoItem({todo}) {
+import { useState } from "react";
+
+function TodoItem({ todo }) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheck = () => {
+        setIsChecked(!isChecked);
+    };
+    
     return (
         <>
             <input
-              type="checkbox"
-              name="done"
-              id=""
-              className="size-5 cursor-pointer rounded border-slate-300 border-b-gray-500 transition-all checked:border-blue-600 checked:bg-blue-400 hover:shadow-md" />
-            <span className="mx-4"
-              >{todo.text}</span
-            >
+                type="checkbox"
+                name="done"
+                id=""
+                className="size-5 cursor-pointer rounded border-slate-300 border-b-gray-500 transition-all checked:border-blue-600 checked:bg-blue-400 hover:shadow-md" checked={isChecked} onChange={handleCheck}/>
+            <span className={`mx-4 ${isChecked ? 'line-through' : ''}`}>
+                {todo.text}
+            </span>
             <div className="ml-auto flex space-x-2">
               <button className="bg-[#29b83a] px-4 py-2 text-white">
                 bearbeiten
